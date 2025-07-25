54f18ad668f8d0e245622e31712b7d05e0ceffd2
josep25062023 <223260@ids.upchiapas.edu.mx>
2025-07-24 20:58:31 -0600

    ultomo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.up_rivals"
+        applicationId = "com.MictlanLabs.UP_rivals"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
